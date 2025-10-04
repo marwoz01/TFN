66c32e1db4a0ef5b1a1fd27149cc6bc9494de14c
Marcel Wozniak <marwozniak01@gmail.com>
2025-10-04 11:58:49 +0200

    Poprawiłem responsywność

--- 01-GameDex/app.js
+++ 01-GameDex/app.js
@@ -4,10 +4,7 @@ async function show20Pokemons() {
   try {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
     const data = await response.json();
-
     pokemonUl.innerHTML = data.results.map(renderPokemon).join("");
-
-    console.log(data);
   } catch (error) {
     console.log(error);
   }
