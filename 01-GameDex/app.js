6bf66ceb296db07f160fa3cfaa24f161babd65d9
Marcel Wozniak <marwozniak01@gmail.com>
2025-10-04 16:04:41 +0200

    Dodałem możliwość wyszukiwania i wyświetlanie pokemona

--- 01-GameDex/app.js
+++ 01-GameDex/app.js
@@ -1,16 +1,49 @@
 const pokemonUl = document.getElementById("pokemon-list");
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
 
-async function show20Pokemons() {
+async function findPokemon() {
   try {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+    const pokemonName = searchInput.value.trim().toLowerCase();
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    if (!response.ok) {
+      return (pokemonUl.innerHTML = `<li>Brak wyników</li>`);
+    }
     const data = await response.json();
-    pokemonUl.innerHTML = data.results.map(renderPokemon).join("");
+    console.log(data);
+    showMatchingPokemons(data);
   } catch (error) {
     console.log(error);
   }
 }
 
-function renderPokemon(data) {
+searchBtn.addEventListener("click", findPokemon);
+
+function showMatchingPokemons(pokemon) {
+  const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+  pokemonUl.innerHTML = `
+  <li>
+    <div class="pokemon-card">
+      <img src="${imgUrl}" alt="${pokemon.name}">
+      <h2>${pokemon.name}</h2>
+    </div>
+  </li>
+`;
+}
+
+async function getDefaultPokemons() {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+    const data = await response.json();
+    pokemonUl.innerHTML = data.results.map(showDefaultPokemons).join("");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function showDefaultPokemons(data) {
   const { name, url } = data;
   const parts = url.split("/");
   const id = parts[parts.length - 2];
@@ -26,4 +59,4 @@ function renderPokemon(data) {
 `;
 }
 
-show20Pokemons();
+getDefaultPokemons();
