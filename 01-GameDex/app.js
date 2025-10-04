f3454f4838cce066ba851f09dbd89f1e2c942d89
Marcel Wozniak <marwozniak01@gmail.com>
2025-10-04 17:55:54 +0200

    Dodałem komunikat ładowania

--- 01-GameDex/app.js
+++ 01-GameDex/app.js
@@ -30,26 +30,28 @@ pokemonUl.addEventListener("click", async (e) => {
     )
     .join("");
 
-  modalBody.innerHTML = `
-  <div class="modal-header">
-    <img src="${imgUrl}" alt="${data.name}">
-    <h3>#${id} ${data.name.toUpperCase()}</h3>
-  </div>
-  <div class="modal-info">
-    <p><strong>Typ:</strong> ${data.types
-      .map((t) => t.type.name)
-      .join(", ")}</p>
-    <p><strong>Wzrost:</strong> ${data.height * 10} cm</p>
-    <p><strong>Waga:</strong> ${data.weight / 10} kg</p>
-    <p><strong>Umiejetnosci:</strong> ${data.abilities
-      .map((a) => a.ability.name)
-      .join(", ")}</p>
-  </div>
-  <h4>Statystyki bazowe</h4>
-  <div class="stats-grid">
-    ${statsGrid}
-  </div>
-`;
+  setTimeout(() => {
+    modalBody.innerHTML = `
+        <div class="modal-header">
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" alt="${
+      data.name
+    }">
+          <h3>#${id} ${data.name.toUpperCase()}</h3>
+        </div>
+        <div class="modal-info">
+          <p><strong>Typ:</strong> ${data.types
+            .map((t) => t.type.name)
+            .join(", ")}</p>
+          <p><strong>Wzrost:</strong> ${data.height * 10} cm</p>
+          <p><strong>Waga:</strong> ${data.weight / 10} kg</p>
+          <p><strong>Umiejętności:</strong> ${data.abilities
+            .map((a) => a.ability.name)
+            .join(", ")}</p>
+        </div>
+        <h4>Statystyki bazowe</h4>
+        <div class="stats-grid">${statsGrid}</div>
+      `;
+  }, 1000);
 });
 
 // Zamknięcie po kliknięciu w krzyżyk
@@ -107,25 +109,25 @@ getDefaultPokemons();
 
 // Funkcja do obsługi wyszukiwania
 async function findPokemon() {
+  const pokemonName = searchInput.value.trim().toLowerCase();
+
+  if (!pokemonName) {
+    getDefaultPokemons();
+    return;
+  }
+
+  pokemonUl.innerHTML = "<li>Ładowanie...</li>";
   try {
-    const pokemonName = searchInput.value.trim().toLowerCase();
-
-    if (!pokemonName) {
-      getDefaultPokemons();
-      return;
-    }
-
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
     );
-
     if (!response.ok) {
       return (pokemonUl.innerHTML = `<li>Brak wyników</li>`);
     }
-
     const data = await response.json();
-    console.log(data);
-    showMatchingPokemons(data);
+    setTimeout(() => {
+      showMatchingPokemons(data);
+    }, 1000);
   } catch (error) {
     console.log(error);
   }
